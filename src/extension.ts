08608649a484e291a7d57fbba02b198afad3209e
tanaka-x <43107252+tanaka-x@users.noreply.github.com>
2018-12-24 00:36:23 +0900

    正常系動作確認完了

--- src/extension.ts
+++ src/extension.ts
@@ -31,46 +31,101 @@ export function activate(context: vscode.ExtensionContext) {
              * ここでテキストを加工します。
              **/
             let text = doc.getText(cur_selection); //取得されたテキスト
-            let lines = text.split(/\r\n|\r|\n/); //改行区切り
+            let properties = text.split(/\r\n|\r|\n/); //改行区切り
 
-            let yaml = "";
-            let oldparams = [""];
-            for (let line of lines) {
-                let space = "";
-                let matchFlag = false;
-                let arrayFlag = false;
-                let sides = line.split("=");
-                for (let i = 2; i < sides.length; i++) { //値に「=」が設定されている場合の応急処置
-                    sides[1] += "=" + sides[i];
-                }
-                let params = sides[0].split(".");
-
-                for (let i = 0; i < params.length; i++) {
-                    if (params[i].match(/\[[0-9]+\]$/)) { //配列の場合
-                        params[i].replace(/\[[0-9]+\]$/, "");
-                        arrayFlag = true;
-                    }
-                    if (oldparams[i] !== params[i]) { //設定値名が一致していないなら移行の項目は出力する
-                        matchFlag = true;
-                    }
-                    if (matchFlag === true) {
-                        let yamlLine = "";
-                        yamlLine += space;
-                        yamlLine += params[i];
-
-                        if (i === params.length - 1) { //最終パラメータ
-                            //console.log(yamlLine + ": " + sides[1]);
-                            yaml += yamlLine + ": " + sides[1] + "\n";
-                        } else { //最終パラメータ
-                            //console.log(yamlLine + ": ");
-                            yaml += yamlLine + ": " + "\n";
-                        }
-                    }
-                    space += "  "; //次行のスペースを設定
-                }
-                oldparams = params; //1行前の情報を保持する
+            for (let i = 0; i < properties.length; i++) { //スペースを除去
+                properties[i] = properties[i].replace(/\s+/g, '');
             }
 
+            let yaml = ""; //加工後のyaml
+            let oldparams = [""]; //1行前のproperties
+
+            for (let propertie of properties) { //propertiesを1行ずつ回す
+                if (propertie !== "") {
+                    let space = ""; //keyの前につける[space]
+                    let matchFlag = true; //key一致フラグ
+                    let sides = propertie.split("="); //key,valueい分割する
+                    for (let i = 2; i < sides.length; i++) { //valueに "=" が設定されている場合の応急処置
+                        sides[1] += "=" + sides[i];
+                    }
+
+                    let params = sides[0].split("."); //keyを "." で分割する
+
+                    for (let i = 0; i < params.length; i++) {
+                        let new1 = params[i];
+                        let new2 = params[i].replace(/\[[0-9]+\]$/, "[]");
+                        let old1 = "";
+                        let old2 = "";
+                        if (typeof (oldparams[i]) !== "undefined") {
+                            old1 = oldparams[i];
+                            old2 = oldparams[i].replace(/\[[0-9]+\]$/, "[]");
+                        }
+                        if (old2 !== new2) { //keyが一致していないなら以降の項目は出力する
+                            matchFlag = false;
+                        }
+                        // 2つ前以前にある配列の数だけ"__"を入れる
+                        let space2 = "";
+                        for (let j = 0; j <= i - 2; j++) { //2つ前以前にある配列の数だけ"  "を入れる
+                            if (params[j].match(/\[[0-9]+\]$/)) { //配列の場合
+                                space2 += "  ";
+                            }
+                        }
+                        // 1つ前が配列なら"-_"か"  "を入れる
+                        let space1 = "";
+                        if (i !== 0) {
+                            if (params[i - 1].match(/\[[0-9]+\]$/)) { //配列の場合
+                                if (typeof (oldparams[i]) !== "undefined" && params[i - 1] === oldparams[i - 1]) {
+                                    space1 += "  ";
+                                } else {
+                                    space1 += "- ";
+                                }
+                            }
+                        }
+                        if (matchFlag === false) {
+                            // 配列の判定
+                            if (params[i].match(/\[[0-9]+\]$/)) {
+                                let replaceParams = params[i].replace(/\[[0-9]+\]$/, ""); //keyの[x]を消す
+                                // 配列の場合
+                                // 最終項目の判定
+                                if (i === params.length - 1) {
+                                    // 最終項目の場合
+                                    yaml += space + space2 + space1 + replaceParams + ":" + "\n"
+                                        + space + "  " + space2 + "- " + sides[1] + "\n";
+                                } else {
+                                    // 最終項目ではない場合
+                                    yaml += space + space2 + space1 + replaceParams + ":" + "\n";
+                                }
+                            } else {
+                                // 配列ではない場合
+                                // 最終項目の判定
+                                if (i === params.length - 1) {
+                                    // 最終項目の場合
+                                    yaml += space + space2 + space1 + params[i] + ":" + " " + sides[1] + "\n";
+                                } else {
+                                    // 最終項目ではない場合
+                                    yaml += space + space2 + space1 + params[i] + ":" + "\n";
+                                }
+                            }
+                        } else {
+                            if (new2 === old2 && new1 !== old1) {
+                                // 配列の判定
+                                if (params[i].match(/\[[0-9]+\]$/)) {
+                                    // 配列の場合
+                                    // 最終項目の判定
+                                    if (i === params.length - 1) {
+                                        // 最終項目の場合
+                                        yaml += space + "  " + space2 + "- " + sides[1] + "\n";
+                                    } else {
+                                        matchFlag = false;
+                                    }
+                                }
+                            }
+                        }
+                        space += "  ";
+                    }
+                    oldparams = params; //1行前の情報を保持する
+                }
+            }
             //エディタ選択範囲にテキストを反映
             editor.edit(edit => {
                 edit.replace(cur_selection, yaml);
